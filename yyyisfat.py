2e82abd70cefd20e01cec58ab148fdc73fb25404
yhr <1099139341@qq.com>
2020-03-25 16:02:55 +0800

    update5

--- yyyisfat.py
+++ yyyisfat.py
@@ -29,3 +29,15 @@ print()
 
 #阿姆斯特朗数
 #如果一个n位正整数等于其各位数字的n次方之和, 则称该数为阿姆斯特朗数。 例如1^3 + 5^3 + 3^3 = 153。
+num = int(input("请输入一个数字: "))
+sum = 0
+n = len(str(num))
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** n
+    temp //= 10
+if num == sum:
+    print(num, "是阿姆斯特朗数")
+else:
+    print(num, "不是阿姆斯特朗数")
